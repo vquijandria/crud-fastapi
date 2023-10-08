493efce84c76a6c3d424a816793132a2f3d0e90a
Vicente Quijandria <vquijandria@hotmail.com>
2023-10-08 05:00:11 -0500

    primer commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vquij\documents\coding\python\crud-fastapi\venv
+set VIRTUAL_ENV=C:\Users\vquij\Documents\coding\Python\crud-fastapi\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
